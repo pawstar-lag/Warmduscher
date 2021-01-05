9a6af4878b5e0691c0401e0748b92068ec9461b3
pawstar-lag <69009801+pawstar-lag@users.noreply.github.com>
2021-01-05 15:01:05 -0800

    Create insults.danish.js

---
+++ Danish/insults.danish.js
@@ -0,0 +1,6 @@
+let insults = [
+ 'Du har jord i hovedet'
+]
+const random = Math.floor(Math.random() * insults.length);
+document.write(random, insults[random]);
+//This will add the number in the list before the insult, so yeah ignore that
