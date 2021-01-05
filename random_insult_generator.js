5a34835f1f0bd909b79789d0cae185a9866da7df
pawstar-lag <69009801+pawstar-lag@users.noreply.github.com>
2021-01-04 21:39:55 -0800

    Update random_insult_generator.js

--- random_insult_generator.js
+++ random_insult_generator.js
@@ -1,6 +1,7 @@
 let insults = [
 'Warmduscher',
- 'Kyss mig i arslet'
+ 'Kyss mig i arslet',
+ 'Du har jord i hovedet'
 ]
 const random = Math.floor(Math.random() * insults.length);
 document.write(random, insults[random]);
