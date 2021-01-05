3ac028d3dceda2dc76775f14f98bbf80ffa1cc42
pawstar-lag <69009801+pawstar-lag@users.noreply.github.com>
2021-01-04 19:28:31 -0800

    Update insults.js

--- German/insults.js
+++ German/insults.js
@@ -1,3 +1,6 @@
 let insults = [
 'Warmduscher'
 ]
+const random = Math.floor(Math.random() * insults.length);
+document.write(random, insults[random]);
+//This will add a '0' before the insult, so ignore that
