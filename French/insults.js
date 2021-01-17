7b6bd3638007e199ae35be4c7f71769b716459b5
私は肉球の星遅れ <69009801+pawstar-lag@users.noreply.github.com>
2021-01-17 09:59:52 -0800

    Create insults.js

---
+++ French/insults.js
@@ -0,0 +1,6 @@
+let insults = [
+ "Il a un poil dans la main"
+]
+const random = Math.floor(Math.random() * insults.length);
+document.write(random, insults[random]);
+//This will add the number in the list before the insult, so ignore that.
