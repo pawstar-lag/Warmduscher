724c096f502d22b923f3977751fb3eadc34cf5b2
肉球の星遅れ <69009801+pawstar-lag@users.noreply.github.com>
2021-02-08 19:25:57 -0800

    Update insults.js

--- Danish/insults.js
+++ Danish/insults.js
@@ -1,6 +1,5 @@
-let insults = [
+let insults = Array(
  'Du har jord i hovedet'
-]
-const random = Math.floor(Math.random() * insults.length);
-document.write(random, insults[random]);
-//This will add the number in the list before the insult, so yeah ignore that
+)
+let randomValue = insults[Math.floor(Math.random() * insults.length)];
+document.write(randomValue);
