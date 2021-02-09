6e944d4231aa22562826fb475add9d89a6d70310
肉球の星遅れ <69009801+pawstar-lag@users.noreply.github.com>
2021-02-08 19:25:08 -0800

    Update insults.js

--- German/insults.js
+++ German/insults.js
@@ -1,7 +1,6 @@
-let insults = [
-'Warmduscher',
-  'Verdammt'
-]
-const random = Math.floor(Math.random() * insults.length);
-document.write(random, insults[random]);
-//This will add the number in the list before the insult, so ignore that.
+ let insults = Array(
+ 'Verdammt',
+'Warmduscher'
+)
+let randomValue = insults[Math.floor(Math.random() * insults.length)];
+document.write(randomValue);
